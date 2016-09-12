97f36dff9e084f5066b1b2107debc8af8a7c049f
JulienChevron <chevron-j@hotmail.fr>
2016-09-12 13:40:50 +0200

    Retrait connexion dans fonctionUtiles

--- php/fonctionsUtiles.php
+++ php/fonctionsUtiles.php
@@ -1,21 +1,5 @@
 <?php
 
-
-	function ConnexionBase(){
-		$host = "https://spartacus.iutc3.unicaen.fr/phpmyadmin/";
-		$user = "julien_chevron";
-		$passwd = "tiiselai";
-		try
-		{
-			$bdd = new PDO('mysql:host=spartacus.iutc3.unicaen.fr/phpmyadmin/;dbname=julien_chevron;', $user, $passwd);
-		}
-		
-		catch (Exception $e)
-		{
-			die('Erreur : ' . $e->getMessage());
-		}
-	}
-
   function verifierRempli($n)
   {  
     if (!empty($_POST[$n]))
