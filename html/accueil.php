93c8ea5074dd465feded234739ea12fd4253c016
xavierefremont <xaviere.fremont@yahoo.fr>
2016-10-07 14:19:14 +0200

    Accueil
    PRESQUE fini

--- html/accueil.php
+++ html/accueil.php
@@ -9,13 +9,36 @@
 	<body>
 	
 		<?php include("header.html"); ?>
-		<div class="presenter">
-			<p>OptiLife est un site de gestion d'emploi du temps et d'optimisation de son temps.</p>
-		</div>
-		<div class="acc">
-			<table class="tabacc" border=1px><tr><td id="tutoriel">TUTORIEL</td><td id="explication">PROJET OptiLife</td></tr> </table>
-			<input id="btn" type=button value="Commencer un projet" onclick="document.location.href='edt.php'"/>
 		
+ 
+	<div class="acc">
+	<section id="optiacc" class="crayon article-css-27 demoTime">
+		<div id="demoWrap">
+			<div id="present">
+				<div id="tuto">
+					<div id="background">
+						<ul>
+	                            <li><a href="#present"><span>Présentation</span></a></li>
+	                            <li><a href="#tuto"><span>Tutoriel</span></a></li>
+	                            
+						</ul>
+	                    <div id="fleche"></div>
+							<div id="textes">
+	                            <p>Plus qu'a mettre la présentation</p>
+								
+	                            <p>Mettre le tutoriel</p>
+	                           
+	                       
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+	</section>
+
+	<input id="btn" type=button value="Commencer un projet" onclick="document.location.href='edt.php'"/>
 			<?php include("connectacc.php"); ?>
 		
 		</div>
