a635600705c00e32d2f011f85cf4e87a489c77ea
JulienChevron <chevron-j@hotmail.fr>
2016-11-17 17:03:24 +0100

    Amélioration formulaire ajout

--- Optilife/php/connexionBDD.php
+++ Optilife/php/connexionBDD.php
@@ -3,7 +3,6 @@ try
 {
 
 	$bdd = new PDO('mysql:host=localhost;dbname=optilife', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
-
 } catch (Exception $e)
 {
 	die('Erreur : ' . $e->getMessage());
