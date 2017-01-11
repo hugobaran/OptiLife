b325c8295db95742be901924cb23055b3e071790
xavierefremont <xaviere.fremont@yahoo.fr>
2017-01-11 20:41:26 +0100

    Connexion D'un utilisateur
    OK pour la connexion avec création d'une variable de session pseudo
    (pour l'instant c'est pas terrible car plsrs personnes peuvent avoir le
    même pseudo) et changement des pages lorsqu'on est connecté : "connexion
    / inscription" deviennent "compte/deconnexion"

--- Optilife/html/connexion.php
+++ Optilife/html/connexion.php
@@ -1,5 +1,32 @@
+<?php session_start();
+ 
+include("../php/connexionBDD.php");
 
-<html>
+include("header.php");
+include("connexion.html");
 
+if(!empty($_POST["pseudo"])){
+	if(!empty($_POST["mdp"])){
+		$pseudo = $_POST["pseudo"];
+		$mdp = $_POST["mdp"];
+		
+		$sql = "SELECT count(*) as nb FROM `inscrit` WHERE `USR_PSEUDO` = '".$pseudo."' AND `USR_MDP` = '".$mdp."';";
+		$stmt =  $bdd->query($sql);
+		$i=0;
+		foreach($stmt as $row){
+		if($i ==0){
+			$nb= $row['nb'];
+			$i++;
+			}
+		}
+		if($nb>0){
+			$_SESSION["usrPseudo"]=$pseudo;
+		}
 
-</html>
+	}
+	else{ }
+}
+
+// traitement du formulaire
+
+?>
