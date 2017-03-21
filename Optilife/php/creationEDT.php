bc9d32c4824a750fd7ba58531991adbf9b3e092a
RemiLecouillard <remizbay@hotmail.fr>
2017-03-21 14:52:14 +0100

    Petit bug quand on change d'edt reglé

--- Optilife/php/creationEDT.php
+++ Optilife/php/creationEDT.php
@@ -18,19 +18,19 @@ if(isset($_POST['creationEDTSubmit'])){
 		}else{
 			$num = 3;
 		}
+		$numEDT = 1;
 		echo $num;
 		if($tab[0]["count(*)"] == 0){
-			$sql = "INSERT INTO `emploidutemps` (`EMP_NUM`, `CAT_NUM`, `USR_NUM_VISITEUR`, `USR_NUM_INSCRIT`) VALUES (1, '".$num."', '".$_SESSION["usrNum"]."', NULL)";
+			$sql = "INSERT INTO `emploidutemps` (`EMP_NUM`, `CAT_NUM`, `USR_NUM_VISITEUR`, `USR_NUM_INSCRIT`) VALUES (".$numEDT.", '".$num."', '".$_SESSION["usrNum"]."', NULL)";
 			$bdd ->exec($sql);
 		}else{
 			$sql = "select max(emp_num)+1 as max from emploidutemps";
 			$tab = LireDonneesPDO1($bdd, $sql);
-			$sql = "INSERT INTO `emploidutemps` (`EMP_NUM`, `CAT_NUM`, `USR_NUM_VISITEUR`, `USR_NUM_INSCRIT`) VALUES (".$tab[0]['max'].", '".$num."', '".$_SESSION["usrNum"]."', NULL)";
+			$numEDT = $tab[0]['max'];
+			$sql = "INSERT INTO `emploidutemps` (`EMP_NUM`, `CAT_NUM`, `USR_NUM_VISITEUR`, `USR_NUM_INSCRIT`) VALUES (".$numEDT.", '".$num."', '".$_SESSION["usrNum"]."', NULL)";
 			$bdd ->exec($sql);
 		}
-	$sql1 = "SELECT * FROM `emploidutemps` WHERE `USR_NUM_VISITEUR` LIKE '".$_SESSION["usrNum"]."' ";
-	$tab = LireDonneesPDO1($bdd, $sql1);
-	$_SESSION["EMP_NUM"] = $tab[0]["EMP_NUM"];
+	$_SESSION["EMP_NUM"] = $numEDT;
 	}
 }
 header("location: ../html/main.php");
