b34a739b782ab46c75f001dacc5abef9ced98b4b
xavierefremont <xaviere.fremont@yahoo.fr>
2017-03-22 15:53:26 +0100

    Inscription/connexion
    Plus d'erreur si pseudo/mdp faux
    A faire : redirection de page

--- Optilife/html/connexion.php
+++ Optilife/html/connexion.php
@@ -8,27 +8,34 @@ if(!isset($_POST["pseudo"])){
 include("connexion.html");
 }
 
-if(!empty($_POST["pseudo"]) && !empty($_POST["mail"])){
+if(!empty($_POST["pseudo"]) ){
 	if(!empty($_POST["mdp"])){
 		$pseudo = $_POST["pseudo"];
 		$mdp = $_POST["mdp"];
 		
 		$sql = "SELECT `USR_NUM` as num,`USR_ADMIN` as adm FROM `inscrit` WHERE `USR_PSEUDO` = '".$pseudo."' AND `USR_MDP` = '".$mdp."'";
 		$stmt =  $bdd->query($sql);
+		$req=$bdd->prepare("SELECT `USR_NUM` as num,`USR_ADMIN` as adm FROM `inscrit` WHERE `USR_PSEUDO` = '".$pseudo."' AND `USR_MDP` = '".$mdp."'");
+		$res = $req->fetchAll();
 		$i=0;
 		foreach($stmt as $row){
-		if($i ==0){
+		if($res == true){
 			$admin= $row['adm'];
 			$num= $row['num'];
 			$i++;
 			}
 			
 		}
-		if(1){
+		if($res == true){
 			$_SESSION["usrNum"]=$num; 
 			$_SESSION["usrPseudo"]=$pseudo;
 			$_SESSION["usrAdmin"]=$admin; 
 			header('location:accueil.php');
+		}
+		else{
+			
+			echo "<script>alert(\"Pseudo ou mot de passe incorrect\")</script>"; 
+			
 		}
 
 	}
